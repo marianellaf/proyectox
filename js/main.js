1637311d107a75bc5dcd3db7eb9b5e4efa7fa3ef
marianella fabrega astudillo <marianella fabrega astudillo>
2016-11-11 12:53:52 -0300

    gh-pages subiendo

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*function messaje(){
+
+	var nuevomessaje ='<div class="w-message w-message-out">'
+
+	document.getElementByid("conversacion").innerhtml += nuevo mensaje.value;
+
+	var mensaje=document.getElementByid('mensajes').value;*/
+
+
+function  creardiv(){
+	var div.document.createElement('div');
+	div.setAttribute('class=','"w-message w-message-in"')
+	var div0.document.createElement('div0')
+	div0.setAttribute('class=','"")
+}
+
